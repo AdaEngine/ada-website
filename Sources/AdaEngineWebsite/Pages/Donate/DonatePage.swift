f58ccb2a1528ae9f8c483710cd8977c301818645
Vladislav Prusakov <spectraldragon@bk.ru>
2025-05-06 03:29:47 +0300

    Good night

--- Sources/AdaEngineWebsite/Pages/Donate/DonatePage.swift
+++ Sources/AdaEngineWebsite/Pages/Donate/DonatePage.swift
@@ -72,6 +72,10 @@ private extension DonatePage {
     enum SectionID: String {
         case usd
         case boosty
+        
+        var button: String {
+            self.rawValue + "-btn"
+        }
     }
     
     func donatePicker() -> some HTML {
@@ -87,6 +91,7 @@ private extension DonatePage {
                     HideElement(SectionID.boosty.rawValue)
                     ShowElement(SectionID.usd.rawValue)
                 }
+                .role(.default)
                 .width(2)
                 
                 Button {
@@ -95,9 +100,10 @@ private extension DonatePage {
                     HideElement(SectionID.usd.rawValue)
                     ShowElement(SectionID.boosty.rawValue)
                 }
+                .role(.default)
                 .width(2)
             }
-            .columns(5)
+            .columns(4)
             .padding(.vertical, 4)
             .frame(width: .percent(100%))
             .border(.white, width: 1.5, cornerRadii: .init(8))
@@ -148,154 +154,4 @@ private extension DonatePage {
             }
         }
     }
-}
-
-extension DonatePage {
-   static let monthlyUSDLevels: [DonationMonthlyLevel] = [
-        .init(
-            name: "Bronze",
-            icon: "/images/ae_logo_white.svg",
-            cost: "$5",
-            benefits: [
-                .nameInCredits
-            ],
-            color: "var(--color-donate-level-bronze-color)",
-            isDark: false,
-            link: "/"
-        ),
-        .init(
-            name: "Silver",
-            icon: "/images/ae_logo_white.svg",
-            cost: "$10",
-            benefits: [
-                .nameInCredits
-            ],
-            color: "var(--color-donate-level-silver-color)",
-            isDark: false,
-            link: "/"
-        ),
-        .init(
-            name: "Gold",
-            icon: "/images/ae_logo_black.svg",
-            cost: "$25",
-            benefits: [
-                .nameInCredits
-            ],
-            color: "var(--color-donate-level-gold-color)",
-            isDark: true,
-            link: "/"
-        ),
-        .init(
-            name: "Platinum",
-            icon: "/images/ae_logo_black.svg",
-            cost: "$50",
-            benefits: [
-                .logoInCredits,
-            ],
-            color: "var(--color-donate-level-platinum-color)",
-            isDark: true,
-            link: "/"
-        ),
-        .init(
-            name: "Titanium",
-            icon: "/images/ae_logo_white.svg",
-            cost: "$100",
-            benefits: [
-                .logoInCredits,
-                .linkInCredits,
-                .supportByTeam
-            ],
-            color: "var(--color-donate-level-titanium-color)",
-            isDark: false,
-            link: "/"
-        ),
-        .init(
-            name: "Diamond",
-            icon: "/images/ae_logo_black.svg",
-            cost: "$250",
-            benefits: [
-                .logoInCredits,
-                .linkInCredits,
-                .supportByTeam
-            ],
-            color: "var(--color-donate-level-diamond-color)",
-            isDark: true,
-            link: "/"
-        )
-    ]
-    
-    static let monthlyBoostyLevels: [DonationMonthlyLevel] = [
-         .init(
-             name: "Bronze",
-             icon: "/images/ae_logo_white.svg",
-             cost: "350₽",
-             benefits: [
-                 .nameInCredits
-             ],
-             color: "var(--color-donate-level-bronze-color)",
-             isDark: false,
-             link: "https://boosty.to/adaengine/purchase/3179294?ssource=DIRECT&share=subscription_link"
-         ),
-         .init(
-             name: "Silver",
-             icon: "/images/ae_logo_white.svg",
-             cost: "800₽",
-             benefits: [
-                 .nameInCredits
-             ],
-             color: "var(--color-donate-level-silver-color)",
-             isDark: false,
-             link: "https://boosty.to/adaengine/purchase/3179295?ssource=DIRECT&share=subscription_link"
-         ),
-         .init(
-             name: "Gold",
-             icon: "/images/ae_logo_black.svg",
-             cost: "1500₽",
-             benefits: [
-                 .nameInCredits
-             ],
-             color: "var(--color-donate-level-gold-color)",
-             isDark: true,
-             link: "https://boosty.to/adaengine/purchase/3179293?ssource=DIRECT&share=subscription_link"
-         ),
-         .init(
-             name: "Platinum",
-             icon: "/images/ae_logo_black.svg",
-             cost: "3500₽",
-             benefits: [
-                 .logoInCredits,
-             ],
-             color: "var(--color-donate-level-platinum-color)",
-             isDark: true,
-             link: "https://boosty.to/adaengine/purchase/3354083?ssource=DIRECT&share=subscription_link"
-         ),
-         .init(
-             name: "Titanium",
-             icon: "/images/ae_logo_white.svg",
-             cost: "5000₽",
-             benefits: [
-                 .logoInCredits,
-                 .linkInCredits,
-                 .supportByTeam,
-                 .merch
-             ],
-             color: "var(--color-donate-level-titanium-color)",
-             isDark: false,
-             link: "https://boosty.to/adaengine/purchase/3354084?ssource=DIRECT&share=subscription_link"
-         ),
-         .init(
-             name: "Diamond",
-             icon: "/images/ae_logo_black.svg",
-             cost: "20000₽",
-             benefits: [
-                 .logoInCredits,
-                 .linkInCredits,
-                 .supportByTeam,
-                 .merch
-             ],
-             color: "var(--color-donate-level-diamond-color)",
-             isDark: true,
-             link: "https://boosty.to/adaengine/purchase/3354085?ssource=DIRECT&share=subscription_link"
-         )
-     ]
 }
